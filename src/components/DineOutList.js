e8cf7d114ceeeae48ea6514742251aa252ee92e6
YKinsbergen <Yannick.kinsbergen@hotmail.com>
2018-08-27 18:05:39 +0200

    Implemented routing for homepage and swipe page, renamed UserList(container) to DineOut(container)

---
+++ src/components/DineOutList.js
@@ -0,0 +1,11 @@
+//components/DineOutList.js
+import * as React from 'react'
+
+export default function DineOutList(props) {
+    return (
+        <div>
+            You're now in component: DineOutList
+            This is where you'll be swiping.
+        </div> 
+    )
+}
