e866fde0a3f9885f54273cb782d15fb704ff65c6
YKinsbergen <Yannick.kinsbergen@hotmail.com>
2018-08-30 16:56:57 +0200

    rudimentary changes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   componentDidMount () {
     setTimeout(() =>
       this.setState({ loading: false })
-    , 200)
+    , 1500)
 
     library.add(faStroopwafel, faUser, faUtensils, faComments, faTicketAlt, faClock, faMapMarkerAlt, faArrowLeft )
   }
