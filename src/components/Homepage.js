82c0d7c9a95ce78054e7a48d402723b26ebdae5f
marleintje <marleinrusch@gmail.com>
2018-08-31 11:20:43 +0200

    improved indendatation for profile sign up page part

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,93 +8,90 @@ import { addProfile } from '../actions/profile'
 
 let dog = false
 class Homepage extends React.Component {
-    handleChange = (event) => {
-        const value = event.target.value
-        const name = event.target.name;
+  handleChange = (event) => {
+    const value = event.target.value
+    const name = event.target.name;
 
     this.setState({
-        [name]: value
+      [name]: value
     })
-}
+  }
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        if (event.target.name.value && event.target.address.value && event.target.photo.value) {
+  handleSubmit = (event) => {
+    event.preventDefault()
+      if (event.target.name.value && event.target.address.value && event.target.photo.value) {
         dog = true,
         this.props.addProfile({
-        name: this.state.name,
-        address: this.state.address,
-        photo: this.state.photo
+          name: this.state.name,
+          address: this.state.address,
+          photo: this.state.photo
         })
         this.forceUpdate() 
-        } else {
-            document.getElementById("submitmessage").innerHTML = 'Please fill in every field.'
-        }
-}
+      } else {
+        document.getElementById("submitmessage").innerHTML = 'Please fill in every field.'
+      }
+  }
 
-    render() {
-        if (dog === true) {
-        return (
-    <div className="App">
-    <div id="message-icon-wrapper">
-        <Link to='/Messages' id="message-link">
+  render() {
+    if (dog === true) {
+    return (
+      <div className="App">
+        <div id="message-icon-wrapper">
+          <Link to='/Messages' id="message-link">
             <img src={messageicon} />
-        <p class="message-icon-text">Messages</p>
-        </Link>
-    </div>
-    <div id="profile-icon-wrapper">
-         <Link to='/Profile' id="profile-link">
-            <img src={profileicon} />
-        <p class="profile-icon-text">Profile</p>
-        </Link>
-    </div>
-    <header className="header"> 
-    <h1>Welcome to <br />COOK or LOOK </h1>
-    </header>
+            <p class="message-icon-text">Messages</p>
+          </Link>
+        </div>
+        <div id="profile-icon-wrapper">
+            <Link to='/Profile' id="profile-link">
+              <img src={profileicon} />
+              <p class="profile-icon-text">Profile</p>
+            </Link>
+        </div>
+        <header className="header"> 
+          <h1>Welcome to <br />COOK or LOOK </h1>
+        </header>
         <div id="homepage-button-div"> 
-            <div id="dine-in-div">
+          <div id="dine-in-div">
             <Link to='/Cooking' id="dine-in-link">
-            Cooking
+              Cooking
             </Link>  
-            </div>
-            <div id="dine-out-div">
+          </div>
+          <div id="dine-out-div">
             <Link to='/Looking' id="dine-out-link">
-            Looking 
+              Looking 
             </Link>    
-            </div> 
+          </div> 
         </div> 
-    </div>
-        )
+      </div>
+    ) 
     } else {
-        return (
-<div align="center">
-
-<h1 id="cooking-header">Welcome!</h1> <br/>
-<h1 id="signup-header">Please create your profile</h1>
-
-<form onSubmit={this.handleSubmit} id="form-div">
-        <div id="form-name">
-            <label>
-                Name: &nbsp;     
-                <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-        </div>
-        <div id="form-name">
-            <label>
-                Address: &nbsp; 
-                <input type="text" name="address" onChange={this.handleChange} />
-            </label>
-        </div>
-        <div id="form-name">
-            <label>
-                 Photo (URL): &nbsp;
-                <input type="text" name="photo" onChange={this.handleChange} />
-            </label>
-        </div>
-                <input type="submit" value="Log-in" id="submit-button"/>
-
-        </form> <br/>
-        <div id="submitmessage"></div>
+      return (
+        <div align="center">
+          <h1 id="cooking-header">Welcome!</h1> <br/>
+          <h1 id="signup-header">Please create your profile</h1>
+          <form onSubmit={this.handleSubmit} id="form-div">
+          <div id="form-name">
+              <label>
+                  Name: &nbsp;     
+                  <input type="text" name="name" onChange={this.handleChange} />
+              </label>
+          </div>
+          <div id="form-name">
+              <label>
+                  Address: &nbsp; 
+                  <input type="text" name="address" onChange={this.handleChange} />
+              </label>
+          </div>
+          <div id="form-name">
+              <label>
+                  Photo (URL): &nbsp;
+                  <input type="text" name="photo" onChange={this.handleChange} />
+              </label>
+          </div>
+          <input type="submit" value="Log-in" id="submit-button"/>
+          </form> <br/>
+          <div id="submitmessage"></div>
         </div>)
     }
 }
---
+++ src/components/JoinDinnerConfirmation.js
@@ -0,0 +1,12 @@
+//components/JoinDinnerConfirmation.js
+import * as React from 'react'
+
+export default class JoinDinnerConfirmation extends React.Component {
+    render() {
+        return (
+            <div>
+                <p> Congratulations! Your dinner at has been confirmed.  </p>
+            </div>
+        )
+    }
+}
